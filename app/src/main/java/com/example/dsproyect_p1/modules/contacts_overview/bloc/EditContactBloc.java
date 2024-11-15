a09d7a39b34e78f9c6a0687f93fbed237470c2b5
SirProg <kevinmaldonadoparedes@gmail.com>
2024-11-15 07:26:01 -0500

    Structure modules

---
+++ app/src/main/java/com/example/dsproyect_p1/modules/contacts_overview/bloc/EditContactBloc.java
@@ -0,0 +1,4 @@
+package com.example.dsproyect_p1.modules.contacts_overview.bloc;
+
+public class EditContactBloc {
+}
--- app/src/main/java/com/example/dsproyect_p1/modules/edit_contact/EditContact.java
+++ app/src/main/java/com/example/dsproyect_p1/modules/edit_contact/EditContact.java
@@ -1,4 +1,4 @@
-package com.example.dsproyect_p1;
+package com.example.dsproyect_p1.modules.edit_contact;
 
 import android.os.Bundle;
 
@@ -8,20 +8,19 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.dsproyect_p1.R;
+
+public class EditContact extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_edit_contact);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-    }
-
-    public void createContacts(){
     }
 }
