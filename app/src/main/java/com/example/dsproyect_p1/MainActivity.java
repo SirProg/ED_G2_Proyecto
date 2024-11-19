13201b0976b03b4ade1125e98fdc2302ef5687d7
SirProg <kevinmaldonadoparedes@gmail.com>
2024-11-18 19:36:25 -0500

    details dinamic

--- app/src/main/java/com/example/dsproyect_p1/MainActivity.java
+++ app/src/main/java/com/example/dsproyect_p1/MainActivity.java
@@ -20,7 +20,8 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.dsproyect_p1.modules.adapter.AdapterRecyclerView;
-import com.example.dsproyect_p1.modules.add_contact.AddContact;
+import com.example.dsproyect_p1.modules.add_contact.AddContactCompany;
+import com.example.dsproyect_p1.modules.add_contact.AddContactPersona;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -63,7 +64,7 @@ public class MainActivity extends AppCompatActivity {
         iBtnPerson.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, AddContact.class);
+                Intent intent = new Intent(MainActivity.this, AddContactPersona.class);
                 startActivity(intent);
             }
         });
@@ -73,7 +74,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 popupWindow.dismiss();
-                Intent intent = new Intent(MainActivity.this, AddContact.class);
+                Intent intent = new Intent(MainActivity.this, AddContactCompany.class);
                 startActivity(intent);
             }
         });
